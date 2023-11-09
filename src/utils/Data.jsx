1bce420e3453d1ab08aa53f42f98c78945fc3044
gokulkrishnan1509 <gokulkrishnan1509@gmail.com>
2023-11-09 18:33:06 +0530

    images recticy

--- src/utils/Data.jsx
+++ src/utils/Data.jsx
@@ -1,27 +1,33 @@
-export const  services = [
+import service1 from "../images/service.png";
+import service2 from "../images/service-02.png";
+import service3 from "../images/service-03.png";
+import service4 from "../images/service-04.png";
+import service5 from "../images/service-05.png";
+
+export const services = [
   {
     title: "Free Shipping",
     tagline: "From all orders over $5 ",
-    image: "images/service.png",
+    image: service1,
   },
   {
     title: "Daily Surprise Offers",
     tagline: "Save upto 25% off",
-    image: "images/service-02.png",
+    image: service2,
   },
   {
     title: "Support 24/7",
     tagline: "Shop with an expert",
-    image: "images/service-03.png",
+    image: service3,
   },
   {
     title: "Affordable Prices",
     tagline: "Get Factory Default Price",
-    image: "images/service-04.png",
+    image: service4,
   },
   {
     title: "Secure Payments",
     tagline: "100% Proctected Payment",
-    image: "images/service-05.png",
+    image: service5,
   },
 ];
