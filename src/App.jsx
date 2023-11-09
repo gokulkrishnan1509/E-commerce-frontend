bdae11c6870037f90317b7ded8db7c5c1d9612a0
gokulkrishnan1509 <gokulkrishnan1509@gmail.com>
2023-11-09 17:38:20 +0530

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import React, { Suspense, lazy } from "react";
+import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-
-const lazyLoading = lazy(() => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(import('./pages/Home'));
-    }, 3000);
-  });
-});
-
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
@@ -28,40 +21,32 @@ import ShippingPolicy from "./pages/ShippingPolicy";
 import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
-
 function App() {
   return (
     <>
       <BrowserRouter basename="/E-commerce-frontend">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Suspense fallback={<div>Loading...</div>}>
-                  <Route index element={<lazyLoading />} />
-                </Suspense>
-              </Layout>
-            }
-          />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="product" element={<OurStore />} />
-          <Route path="blogs" element={<Blog />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="product/:id" element={<SingleProduct />} />
-          <Route path="compare-product" element={<CompareProduct />} />
-          <Route path="wishlist" element={<WishList />} />
-          <Route path="login" element={<Login />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="reset-password" element={<ResetPassword />} />
-          <Route path="blog/:id" element={<SingleBlog />} />
-          <Route path="privacy-policy" element={<PrivaPolicy />} />
-          <Route path="term-conditions" element={<TermAndCondition />} />
-          <Route path="refund-policy" element={<RefundPolicy />} />
-          <Route path="shipping-policy" element={<ShippingPolicy />} />
-          <Route path="checkout" element={<Checkout />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="product" element={<OurStore />} />
+            <Route path="blogs" element={<Blog />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="product/:id" element={<SingleProduct />} />
+            <Route path="compare-product" element={<CompareProduct />} />
+            <Route path="wishlist" element={<WishList />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
+            <Route path="privacy-policy" element={<PrivaPolicy />} />
+            <Route path="term-conditions" element={<TermAndCondition />} />
+            <Route path="refund-policy" element={<RefundPolicy />} />
+            <Route path="shipping-policy" element={<ShippingPolicy />} />
+            <Route path="checkout" element={<Checkout />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
